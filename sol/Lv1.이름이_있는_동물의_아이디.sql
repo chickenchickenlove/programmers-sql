73ad8525335019ecafe1c84ad1c935379c918717
chickenchickenlove <ojt90902@naver.com>
2023-04-08 18:15:50 +0900

    04/08

---
+++ sol/Lv1.이름이_있는_동물의_아이디.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59405
+SELECT NAME
+FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1
---
+++ sol/Lv1.최대값_구하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+--https://school.programmers.co.kr/learn/courses/30/lessons/59415
+SELECT DATETIME as '시간'
+FROM ANIMAL_INS
+ORDER BY DATETIME DESC
+limit 1
